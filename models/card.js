875823f11c1f2658fc43286eb162220d022104cc
Artem <artemjay@yandex.ru>
2023-09-04 14:19:20 +0300

    Feat: инициализация проекта и создание базовых моделей БД

---
+++ models/card.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const cardSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: [{
+    type: Schema.Types.ObjectId,
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = model('card', cardSchema);
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+  },
+});
+
+module.exports = model('user', userSchema);
